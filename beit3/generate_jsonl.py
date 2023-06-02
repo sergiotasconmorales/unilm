0d50d503caf0d18d3113aec01281d38f53222fb6
Sergio Tascon Morales <s.tasconmorales@gmail.com>
2023-06-02 17:39:39 +0200

    Quickie

--- beit3/generate_jsonl.py
+++ beit3/generate_jsonl.py
@@ -1,7 +1,7 @@
 from datasets import VQAv2Dataset
 from transformers import XLMRobertaTokenizer
 
-tokenizer = XLMRobertaTokenizer("/your_beit3_model_path/beit3.spm")
+tokenizer = XLMRobertaTokenizer("/storage/workspaces/artorg_aimi/ws_00000/sergio/beit3/tokenizer/beit3.spm")
 
 VQAv2Dataset.make_dataset_index(
     data_path="/storage/workspaces/artorg_aimi/ws_00000/sergio/coco",
